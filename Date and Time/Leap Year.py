114817316bbc4994fe09a8e5742927e5a5a4200c
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2022-11-05 12:22:39 +0530

    Delete Date and Time directory

--- Date and Time/Leap Year.py
+++
@@ -1,8 +0,0 @@
-import calendar
-
-date = input("Enter the date in format(dd/mm/yyyy)\n")
-yyyy = int(date[6:])
-if calendar.isleap(yyyy) :
-    print("The year is a leap year!")
-else:
-    print("The year isn't a leap year!")
--- Date and Time/Stop Watch.py
+++
@@ -1,9 +0,0 @@
-sh, sm, ss =map(int, input("Start time \n").split(":")) 
-eh, em, es =map(int, input("End time \n").split(":"))
-
-
-s = es - ss
-m = (em - sm) * 60
-h = (eh - sh) * 60 * 60
-total = h + m + s
-print("Time in milliseconds is %d"%(total * 1000))
